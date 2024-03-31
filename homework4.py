986d9e1e2cc1bb03cb4783a326fd8e903d1a03e5
Pushinka5 <131241825+Pushinka5@users.noreply.github.com>
2024-04-01 05:46:19 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+immutable_var = 3, 2, 5, 'white'
+print(immutable_var)
+print(immutable_var.upper()) #кортеж не изменяем, может хранить внутри себя изменяему объекты. Например список.
+mutable_list = [2, 5, 3, "blac"]
+print(mutable_list)
+mutable_list [1] = 'red'
+print(mutable_list)
