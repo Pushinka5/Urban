bab960c711503c9cb088db84926e3da62bb5192a
Pushinka5 <131241825+Pushinka5@users.noreply.github.com>
2024-04-01 07:39:33 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'peach', 'straweberry', 'pineapple', 'bannana', 'orange']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'kiwi'
+print(my_list)
+my_dict = {'window':'окно', 'door':'дверь', 'table':'стол'}
+print(my_dict)
+print(my_dict.keys())
+my_dict.update({'chair': 'стул', 'dresser': 'комод'})
+print(my_dict)
