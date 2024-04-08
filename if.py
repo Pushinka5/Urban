a144d51e785ecd67f2ced29aaf35ed4a71c6fc53
Pushinka5 <131241825+Pushinka5@users.noreply.github.com>
2024-04-08 23:31:04 +0700

    Add files via upload

---
+++ if.py
@@ -0,0 +1,37 @@
+x = 29
+print('добрый день!')
+if x < 0:
+    print('меньше нуля')
+print('Прощайте')
+
+a, b = 12, 7
+
+if a < b:
+   print('провал')
+
+if a > b and a > 0:
+    print('cool')
+
+if (a > b) and (a > 0 or b < 500):
+    print('cool')
+
+if 7 < b and b < 12:
+    print('cool')
+
+if '25' > '248':
+    print('cool')
+
+if '369' > '38':
+    print('cool')
+
+if [2,3] > [1,1]:
+    print('cool')
+
+if '8' > 9:
+    print('cool')
+
+if [8,3] > 4:
+    print('cool')
+
+if '8' != 6:
+    print('cool')
